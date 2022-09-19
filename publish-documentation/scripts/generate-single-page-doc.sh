e6103c0c8e6f4ee7762b44268eacd2ab3e6d07e0
Janani Subbiah <janani.subbiah@detroitlabs.com>
2022-09-19 15:04:26 -0400

    Updated the script that generates the single page HTML (#6)

--- publish-documentation/scripts/generate-single-page-doc.sh
+++ publish-documentation/scripts/generate-single-page-doc.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env sh
 
-mkdir build
-cd build || exit
-BUILDDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+go install github.com/nocd5/md2html@latest
 
-mkdir bin
-echo "$BUILDDIR/bin"
-GOBIN="$BUILDDIR/bin" go install github.com/nocd5/md2html@latest
-
-bin/md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
+md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
 -c $PATH_TO_CSS_FILE \
 -o documentation.html
 
