82035cf5db8cfd398bd2da205a05968cfe0cd6fe
Janani Subbiah <janani.subbiah@detroitlabs.com>
2022-09-21 11:01:28 -0400

    Printing file path of doc file

--- publish-documentation/scripts/generate-single-page-doc.sh
+++ publish-documentation/scripts/generate-single-page-doc.sh
@@ -6,5 +6,7 @@ md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
 -c $PATH_TO_CSS_FILE \
 -o documentation.html
 
+readlink -f documentation.html
+
 sed -i.tmp -e 's/<a href=\"[^http|https|#].*\">\(.*\)<\/a>/\1/gi' documentation.html
 sed -i.tmp -e 's/<title>index<\/title>/<title>$PAGE_TITLE<\/title>/gi' documentation.html
