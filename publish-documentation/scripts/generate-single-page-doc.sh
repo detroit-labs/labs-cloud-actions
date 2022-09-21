2645cc2472fd39cd3f823f5fe4ca8198c0f11880
Janani Subbiah <janani.subbiah@detroitlabs.com>
2022-09-21 12:49:20 -0400

    Updated doc path

--- publish-documentation/scripts/generate-single-page-doc.sh
+++ publish-documentation/scripts/generate-single-page-doc.sh
@@ -3,8 +3,8 @@
 go install github.com/nocd5/md2html@latest
 
 md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
--c $PATH_TO_GITHUB_ACTION/$PATH_TO_CSS_FILE \
--o $PATH_TO_GITHUB_ACTION/documentation.html
+-c $PATH_TO_CSS_FILE \
+-o documentation.html
 
-sed -i.tmp -e 's/<a href=\"[^http|https|#].*\">\(.*\)<\/a>/\1/gi' $PATH_TO_GITHUB_ACTION/documentation.html
-sed -i.tmp -e 's/<title>index<\/title>/<title>$PAGE_TITLE<\/title>/gi' $PATH_TO_GITHUB_ACTION/documentation.html
+sed -i.tmp -e 's/<a href=\"[^http|https|#].*\">\(.*\)<\/a>/\1/gi' documentation.html
+sed -i.tmp -e 's/<title>index<\/title>/<title>$PAGE_TITLE<\/title>/gi' documentation.html
