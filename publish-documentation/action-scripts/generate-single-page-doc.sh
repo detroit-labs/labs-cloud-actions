f83a2e3fd3588cc14a4c5a55935fd63d84f8cea9
Jason P <ickybodclay@users.noreply.github.com>
2022-10-14 11:00:29 -0400

    Update generate-single-page-doc.sh

--- publish-documentation/action-scripts/generate-single-page-doc.sh
+++ publish-documentation/action-scripts/generate-single-page-doc.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-# this is a workaround to explicitily state version wanted, more info here: https://maelvls.dev/go111module-everywhere/
 # do not use `go install github.com/nocd5/md2html@latest` as this will install incorrect version
-GO111MODULE=on go get github.com/nocd5/md2html@v2.0.0
+go install github.com/nocd5/md2html@v2.0.0
 
 md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
 -c $PATH_TO_CSS_FILE \
