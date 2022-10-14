990d82a106c7e8dbcf58417591cc11712bd48a5d
Jason P <ickybodclay@users.noreply.github.com>
2022-10-14 11:38:53 -0400

    Update generate-single-page-doc.sh (#22)

--- publish-documentation/action-scripts/generate-single-page-doc.sh
+++ publish-documentation/action-scripts/generate-single-page-doc.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-# do not use `go install github.com/nocd5/md2html@latest` as this will install incorrect version
-go install github.com/nocd5/md2html@v2.0.0
+go install github.com/nocd5/md2html@latest
 
 md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
 -c $PATH_TO_CSS_FILE \
