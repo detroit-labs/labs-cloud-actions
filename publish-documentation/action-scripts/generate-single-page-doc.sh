dab3861b28120f15cbcab377090d77370ff5db45
Jason P <ickybodclay@users.noreply.github.com>
2022-11-07 12:45:17 -0500

    Update generate-single-page-doc.sh
    remove github links from docs as they will not be accessible to anyone outside of Detroit Labs

--- publish-documentation/action-scripts/generate-single-page-doc.sh
+++ publish-documentation/action-scripts/generate-single-page-doc.sh
@@ -6,5 +6,6 @@ md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
 -c $PATH_TO_CSS_FILE \
 -o $DOCUMENTATION_FILE_NAME
 
-sed -i.tmp -e 's/<a href=\"[^http|https|#].*\">\(.*\)<\/a>/\1/gi' $DOCUMENTATION_FILE_NAME
-sed -i.tmp -e 's/<title>index<\/title>/<title>$PAGE_TITLE<\/title>/gi' $DOCUMENTATION_FILE_NAME
+sed -i.tmp -e 's/<a href=\"[^http|https|#].*\">\(.*\)<\/a>/\1/gi' $DOCUMENTATION_FILE_NAME # remove relative links
+sed -i.tmp -e 's/<a href=\"https:\/\/github\.com.*\">(.*)<\/a>/\1/gi' $DOCUMENTATION_FILE_NAME # remove github links
+sed -i.tmp -e 's/<title>index<\/title>/<title>$PAGE_TITLE<\/title>/gi' $DOCUMENTATION_FILE_NAME # set page title
