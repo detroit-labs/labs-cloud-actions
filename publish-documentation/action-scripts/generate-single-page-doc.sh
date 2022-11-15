75d179bb99709540231dc9331f730fb85d2c3897
Jason P <ickybodclay@users.noreply.github.com>
2022-11-15 13:43:49 -0500

    Update generate-single-page-doc.sh
    Added missing sed flag.

--- publish-documentation/action-scripts/generate-single-page-doc.sh
+++ publish-documentation/action-scripts/generate-single-page-doc.sh
@@ -6,6 +6,6 @@ md2html -e -t $LIST_OF_PATHS_TO_MARKDOWN_FILES \
 -c $PATH_TO_CSS_FILE \
 -o $DOCUMENTATION_FILE_NAME
 
-sed -E 's|<a href=".[^.]*/(.*)?/(.*).md">(.*)</a>|<a href="#\L\1-\L\2">\3</a>|' $DOCUMENTATION_FILE_NAME # update relative markdown file links to headings (note SED \L only works on GNU version of SED and not on mac)
-sed -E 's|<a href="https://github.com.*">(.*)</a>|\1|' $DOCUMENTATION_FILE_NAME # remove github links
-sed -E 's|<title>index</title>|<title>$PAGE_TITLE</title>|' $DOCUMENTATION_FILE_NAME # set page title
+sed -i -E 's|<a href=".[^.]*/(.*)?/(.*).md">(.*)</a>|<a href="#\L\1-\L\2">\3</a>|' $DOCUMENTATION_FILE_NAME # update relative markdown file links to headings (note SED \L only works on GNU version of SED and not on mac)
+sed -i -E 's|<a href="https://github.com.*">(.*)</a>|\1|' $DOCUMENTATION_FILE_NAME # remove github links
+sed -i -E 's|<title>index</title>|<title>$PAGE_TITLE</title>|' $DOCUMENTATION_FILE_NAME # set page title
